df1e60c56e7b72487f06a02aa8bf45dee463e657
Monte Davityan <monted97@gmail.com>
2020-03-07 11:49:19 -0800

    added recomendation box

--- app.py
+++ app.py
@@ -8,13 +8,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+def _generate_advice():
+    #these are example advice. Obviusoly the real life service would have more detailed and better advice
+    advice = ["Good job eating healthy but start excersing for more hours...", "Someone has contracted the Coronavirus in your average location. Wash your hands frequently. Eat healthy and keep your immune up.", "It seems you are spending too much time on Youtube. I recommend setting an alarm and establishing a strict watch time."]
+    return advice
+
 @app.route('/overall')
 def overall():
     overall_graph = _overall_graph()
     health_graph = _health_graph()
     finance_graph = _finance_graph()
     productivity_graph = _productivity_graph()
-    return render_template('overall.html', overall_plot=overall_graph, health_graph=health_graph, finance_graph=finance_graph, productivity_graph=productivity_graph)
+    advice = _generate_advice()
+    return render_template('overall.html', overall_plot=overall_graph, health_graph=health_graph, finance_graph=finance_graph, productivity_graph=productivity_graph, advice=advice)
 
 @app.route('/feature')
 def feature():
