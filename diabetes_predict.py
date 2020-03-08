a98caa0cca1719ee57fe3ff754189a25221be2a4
Arm627 <37284620+Arm627@users.noreply.github.com>
2020-03-07 18:28:34 -0800

    Update diabetes_predict.py

--- diabetes_predict.py
+++ diabetes_predict.py
@@ -1,6 +1,6 @@
 import pickle
 from sklearn.neural_network import MLPClassifier
 
-user_data = 
+user_data = [[5, 147, 75, 34, 0, 0.6, 52]]
 model = pickle.load(open("diabetes.p", "rb"))
 model.predict(user_data)
